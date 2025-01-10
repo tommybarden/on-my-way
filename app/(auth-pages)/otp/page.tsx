b85b278dda9e67a218f139b204c494bb0cc81bde
Tommy <tommy.barden@lagtinget.ax>
2025-01-10 11:56:56 +0200

    Added css

--- app/(auth-pages)/otp/page.tsx
+++ app/(auth-pages)/otp/page.tsx
@@ -1,46 +1,54 @@
 "use client"
-import { signInWithOtp, getOTP } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
-import { SubmitButton } from "@/components/submit-button";
-import { Button } from "@/components/ui/button";
-import { PhoneInput } from 'react-international-phone';
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useState } from "react";
+import {getOTP, signInWithOtp} from "@/app/actions";
+import {Message} from "@/components/form-message";
+import {SubmitButton} from "@/components/submit-button";
+import {Button} from "@/components/ui/button";
+import {defaultCountries, PhoneInput} from 'react-international-phone';
+import {Input} from "@/components/ui/input";
+import {Label} from "@/components/ui/label";
+import {useState} from "react";
+import 'react-international-phone/style.css';
 
 export default function Otp(props: { searchParams: Promise<Message> }) {
-  //const searchParams = props.searchParams;
-  const [phoneNumber, setPhoneNumber] = useState('');
-  //const [otp, setOtp] = useState('');
+    //const searchParams = props.searchParams;
+    const [phoneNumber, setPhoneNumber] = useState('');
+    //const [otp, setOtp] = useState('');
 
-  return (
-    <form className="flex-1 flex flex-col min-w-64">
-      <h1 className="text-2xl font-medium">Login</h1>
-      <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Label htmlFor="phone">Telefonnummer</Label>
-        <PhoneInput 
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e)}
-            name="phone" 
-            placeholder="+358123456789" 
-            defaultCountry="fi"
-            hideDropdown
-            required 
-        />
+    const countries = defaultCountries.filter((country) => {
+        return country[1] === 'fi'
+    });
 
-        <Button type="button" onClick={() => getOTP(phoneNumber)}>Skicka kod</Button>
-        </div>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-        <Input
-            type="number"
-            name="otp"
-            placeholder="Din kod"
-            required
+    return (
+        <form className="flex-1 flex flex-col min-w-64 gap-2">
+            <h1 className="text-2xl font-medium">Login</h1>
+            <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+                <Label htmlFor="phone">Telefonnummer</Label>
+                <PhoneInput
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e)}
+                    name="phone"
+                    placeholder="+358123456789"
+                    defaultCountry="fi"
+                    countries={countries}
+                    hideDropdown
+                    required
+                    className="w-full flex"
+                    forceDialCode
+                    inputClassName="flex-1"
+                />
 
-        />
-        <SubmitButton pendingText="Loggar in..." formAction={signInWithOtp}>Logga in</SubmitButton>
+                <Button type="button" onClick={() => getOTP(phoneNumber)}>Skicka kod</Button>
+            </div>
+            <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+                <Input
+                    type="number"
+                    name="otp"
+                    placeholder="Din kod"
+                    required
+                />
+                <SubmitButton pendingText="Loggar in..." formAction={signInWithOtp}>Logga in</SubmitButton>
 
-      </div>
-    </form>
-  );
+            </div>
+        </form>
+    );
 }
