d93d7dc26ddc5419ab289927b4dfa34458ef841e
tommybarden <tommy.barden@gmail.com>
2025-01-20 23:42:53 +0200

    Changed SW generation

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
-// next.config.js
-const withPWA = require('next-pwa')({
-  dest: 'public', // Output directory for service worker
+import withPWA from "next-pwa";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  //reactStrictMode: true,
+};
+
+export default withPWA({
+  ...nextConfig,
+  dest: "public", // Output directory for service worker
   register: true, // Automatically register service worker
   skipWaiting: true, // Activate service worker immediately
-  scope: '/app',
-  sw: 'service-worker.js',
-  disable: process.env.NODE_ENV === 'development', // Disable in development mode
-});
-
-module.exports = withPWA({
-  reactStrictMode: true,
+  scope: "/app",
+  sw: "service-worker.js",
+  disable: process.env.NODE_ENV === "development", // Disable in development mode
 });
