ec8e57c49e7f49c34deb11e122e79908b84d1d42
Tommy <tommy.barden@lagtinget.ax>
2025-01-21 13:44:31 +0200

    Bugfixed to get build through

--- app/api/save-subscription/route.ts
+++ app/api/save-subscription/route.ts
@@ -1,7 +1,7 @@
 import {NextResponse} from "next/server";
 import {createClient} from "@supabase/supabase-js";
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE!);
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
 export async function POST(req: Request) {
     const {userId, subscription} = await req.json();
