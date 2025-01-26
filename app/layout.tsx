48d427bd36883f9f0df31112a7d770bfcafe0bc9
tommybarden <tommy.barden@gmail.com>
2025-01-26 23:14:27 +0200

    Corrected scrollbar and added tag in comments

--- app/layout.tsx
+++ app/layout.tsx
@@ -113,13 +113,26 @@ export default function RootLayout({
 }) {
     return (
         <html lang="sv" className={GeistSans.className} suppressHydrationWarning>
-            <body className="bg-background text-foreground">
+            <body className="bg-background text-foreground overflow-x-hidden">
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="system"
                     enableSystem
                     disableTransitionOnChange
                 >
+
+                    <div dangerouslySetInnerHTML={{
+                        __html: `<!-- 
+
+                    ██  ██████  ███    ███  █████  ██       █████      ███████ ██████  ██   ██ 
+                    ██ ██    ██ ████  ████ ██   ██ ██      ██   ██     ██      ██   ██ ██  ██  
+                    ██ ██    ██ ██ ████ ██ ███████ ██      ███████     █████   ██████  █████   
+               ██   ██ ██    ██ ██  ██  ██ ██   ██ ██      ██   ██     ██      ██   ██ ██  ██  
+                █████   ██████  ██      ██ ██   ██ ███████ ██   ██     ██      ██████  ██   ██ 
+                                                                                               
+                    -->`
+                    }} />
+
                     <main className="min-h-screen flex flex-col items-center">
                         <div className="flex-1 w-full flex flex-col gap-2 items-center">
                             <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
