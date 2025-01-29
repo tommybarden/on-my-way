4d3dac0999d5a2240c95ac8c1eda49fb7e775d2d
tommybarden <tommy.barden@gmail.com>
2025-01-29 18:12:30 +0200

    Added log to DB

--- app/api/station-event/[event]/route.ts
+++ app/api/station-event/[event]/route.ts
@@ -1,18 +1,30 @@
-import {NextResponse} from "next/server";
-import {sendNotification} from "@/services/notifications";
+import { NextResponse } from "next/server";
+//import {sendNotification} from "@/services/notifications";
+import { createClient } from "@/utils/supabase/server";
 
-export async function GET(request: Request, {params}: { params: Promise<{ event: string }> }) {
+export async function GET(request: Request, { params }: { params: Promise<{ event: string }> }) {
     const event = (await params).event
 
     if (!event) {
-        return NextResponse.json({error: "Unauthorized"}, {status: 406});
+        return NextResponse.json({ error: "Unauthorized" }, { status: 406 });
     }
 
-    try {
-        const result = await sendNotification(event, 'TEST');
-        return NextResponse.json({message: "Push-notiser skickade!", result}, {status: 200});
-    } catch (err) {
-        console.error("❌ Fel vid skickning av notiser:", err);
-        return NextResponse.json({error: "Misslyckades att skicka notiser"}, {status: 500});
-    }
+    const supabase = await createClient();
+
+    const { data, error } = await supabase
+        .from('Log')
+        .insert([
+            { category: 'station-event', message: event },
+        ])
+        .select()
+
+    return NextResponse.json({ message: "OK", data }, { status: 200 });
+
+    // try {
+    //     const result = await sendNotification(event, 'TEST');
+    //     return NextResponse.json({message: "Push-notiser skickade!", result}, {status: 200});
+    // } catch (err) {
+    //     console.error("❌ Fel vid skickning av notiser:", err);
+    //     return NextResponse.json({error: "Misslyckades att skicka notiser"}, {status: 500});
+    // }
 }
--- app/api/unit-starting/[id]/route.ts
+++ app/api/unit-starting/[id]/route.ts
@@ -1,19 +1,31 @@
-import {NextResponse} from "next/server";
-import {sendNotification} from "@/services/notifications";
+import { createClient } from "@/utils/supabase/server";
+import { NextResponse } from "next/server";
+//import {sendNotification} from "@/services/notifications";
 
-export async function GET(request: Request, {params}: { params: Promise<{ id: string }> }) {
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
     const id = (await params).id
     const idRegex = /^[A-Za-z]{1,3}\d{2,3}$/;
 
     if (!idRegex.test(id)) {
-        return NextResponse.json({error: "Unauthorized"}, {status: 406});
+        return NextResponse.json({ error: "Unauthorized" }, { status: 406 });
     }
 
-    try {
-        const result = await sendNotification(id + ' startar', 'TEST');
-        return NextResponse.json({message: "Push-notiser skickade!", result}, {status: 200});
-    } catch (err) {
-        console.error("❌ Fel vid skickning av notiser:", err);
-        return NextResponse.json({error: "Misslyckades att skicka notiser"}, {status: 500});
-    }
+    const supabase = await createClient();
+
+    const { data, error } = await supabase
+        .from('Log')
+        .insert([
+            { category: 'unit-starting', message: id },
+        ])
+        .select()
+
+    return NextResponse.json({ message: "OK", data }, { status: 200 });
+
+    // try {
+    //     const result = await sendNotification(id + ' startar', 'TEST');
+    //     return NextResponse.json({message: "Push-notiser skickade!", result}, {status: 200});
+    // } catch (err) {
+    //     console.error("❌ Fel vid skickning av notiser:", err);
+    //     return NextResponse.json({error: "Misslyckades att skicka notiser"}, {status: 500});
+    // }
 }
