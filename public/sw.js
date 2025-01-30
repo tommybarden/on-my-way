c59bdd1639665451e60a962593e5ec0d74c10bb2
tommybarden <tommy.barden@gmail.com>
2025-01-30 19:29:34 +0200

    Updated notificationclick

--- public/sw.js
+++ public/sw.js
@@ -14,7 +14,19 @@ self.addEventListener("push", (event) => {
 
 self.addEventListener("notificationclick", (event) => {
     event.notification.close();
-    if (event.notification.data) {
-        event.waitUntil(clients.openWindow(event.notification.data));
-    }
+
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(
+        clients
+            .matchAll({
+                type: "window",
+            })
+            .then((clientList) => {
+                for (const client of clientList) {
+                    if (client.url === "/" && "focus" in client) return client.focus();
+                }
+                if (clients.openWindow) return clients.openWindow("/profile");
+            }),
+    );
 });
