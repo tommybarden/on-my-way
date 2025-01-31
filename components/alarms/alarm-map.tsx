94f00546e4cd4c4799d058344a56b95efdfd0b3d
Tommy <tommy.barden@lagtinget.ax>
2025-01-31 16:26:15 +0200

    Added background maps

--- components/alarms/alarm-map.tsx
+++ components/alarms/alarm-map.tsx
@@ -1,24 +1,68 @@
 'use client'
-import { useEffect, useRef } from 'react';
+import {useEffect, useRef} from 'react';
 import 'ol/ol.css';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import {Icon, Style} from 'ol/style';
+import {XYZ} from "ol/source";
 
 export default function AlarmMap() {
     const map1Container = useRef();
-    // on component mount create the map and set the map refrences to the state
+
     useEffect(() => {
-        const osmLayer = new TileLayer({
-            source: new OSM(),
+        const center = [2220244.6089087133, 8435829.42412169];
+        // ol.proj.fromLonLat([12.550343, 55.665957])
+
+        const apiKey = "24d8f276-3b42-4630-b218-4d0619e8fb1f";
+
+        // const osmLayer = new TileLayer({
+        //     source: new OSM(),
+        // });
+        //
+        // const taustakartta = new TileLayer({
+        //     source: new XYZ({
+        //         attributions: '&copy; <a target="_blank" href="https://www.maanmittauslaitos.fi/sv/kartor-och-geodata/expertanvandare/terrangdata-och-anskaffning-av-dem">LMV</a>',
+        //         url: 'https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/taustakartta/default/WGS84_Pseudo-Mercator/{z}/{y}/{x}.png?api-key=' + apiKey,
+        //     }),
+        //     opacity: 0.9,
+        //     preload: 0,
+        // })
+
+        const maastokartta = new TileLayer({
+            source: new XYZ({
+                attributions: '&copy; <a target="_blank" href="https://www.maanmittauslaitos.fi/sv/kartor-och-geodata/expertanvandare/terrangdata-och-anskaffning-av-dem">LMV</a>',
+                url: 'https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/maastokartta/default/WGS84_Pseudo-Mercator/{z}/{y}/{x}.png?api-key=' + apiKey,
+            }),
+            opacity: 0.8,
+        })
+
+        const marker = new Feature({
+            geometry: new Point(center),
+        });
+
+        marker.setStyle(new Style({
+            image: new Icon({
+                anchor: [0.5, 1],
+                src: '/marker.png',
+            }),
+        }));
+
+        const vectorLayer = new VectorLayer({
+            source: new VectorSource({
+                features: [marker],
+            }),
         });
 
         const map = new Map({
             target: "map-container",
-            layers: [osmLayer],
+            layers: [maastokartta, vectorLayer],
             view: new View({
-                center: [2220244.6089087133, 8435829.42412169],
+                center,
                 zoom: 15,
             }),
         });
@@ -27,9 +71,5 @@ export default function AlarmMap() {
 
     }, []);
 
-    return (
-        <>
-            <div className="min-w-6xl h-[50rem]" id="map-container" />
-        </>
-    );
+    return <div className="min-w-6xl h-[50rem] bg-white" id="map-container"/>;
 };
