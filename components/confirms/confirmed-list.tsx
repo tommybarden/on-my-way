7dcf781ddff12b16e84274d902ce4685e6ca8ce3
Tommy <tommy.barden@lagtinget.ax>
2025-01-31 15:58:28 +0200

    Redesigned list

--- components/confirms/confirmed-list.tsx
+++ components/confirms/confirmed-list.tsx
@@ -1,14 +1,14 @@
 'use client'
 
-import { calculateConfirmed, getConfirmed } from "@/services/alarms";
-import { removeSubscription, subscribeToConfirmed } from "@/services/subscriptions";
-import { User } from "@/utils/types";
-import { Clock, Flame, Landmark, Merge, MoveRight, Truck, UserCheck } from "lucide-react";
-import { useEffect, useState } from "react";
+import {calculateConfirmed, getConfirmed} from "@/services/alarms";
+import {removeSubscription, subscribeToConfirmed} from "@/services/subscriptions";
+import {User} from "@/utils/types";
+import {Flame, Truck} from "lucide-react";
+import {useEffect, useState} from "react";
 
 export default function ConfirmedList(props: { users: Record<string, User>; alarmId: number; className?: string; }) {
     const [confirmed, setConfirmed] = useState<any[]>([]);
-    const { users, alarmId, className } = props;
+    const {users, alarmId, className} = props;
 
     useEffect(() => {
         if (!alarmId) return;
@@ -52,14 +52,13 @@ export default function ConfirmedList(props: { users: Record<string, User>; alar
         const interval = setInterval(() => {
             setConfirmed(prevState => {
                 if (!prevState) return prevState;
-                const updatedData = prevState.map(row => {
-                    const updatedRow = { ...row };
+                return prevState.map(row => {
+                    const updatedRow = {...row};
                     if (updatedRow.timeLeft > 0) {
                         updatedRow.timeLeft -= 1;  // Minska timeLeft varje sekund
                     }
                     return updatedRow;
                 });
-                return updatedData;
             });
         }, 1000);
 
@@ -83,9 +82,18 @@ export default function ConfirmedList(props: { users: Record<string, User>; alar
     };
 
     const groups = [
-        { title: "far direkt", icon: Merge, color: "text-blue-500", data: confirmed.filter(c => c.minutes < 0) },
-        { title: "på station", icon: UserCheck, color: "text-green-500", data: confirmed.filter(c => c.minutes >= 0 && c.timeLeft <= 0) },
-        { title: "på väg", icon: Clock, color: "text-orange-500", data: confirmed.filter(c => c.timeLeft > 0).sort((a, b) => a.timeLeft - b.timeLeft) }
+        {
+            title: "Far direkt",
+            data: confirmed.filter(c => c.minutes < 0)
+        },
+        {
+            title: "På station",
+            data: confirmed.filter(c => c.minutes >= 0 && c.timeLeft <= 0)
+        },
+        {
+            title: "På väg",
+            data: confirmed.filter(c => c.timeLeft > 0).sort((a, b) => a.timeLeft - b.timeLeft)
+        }
     ];
 
     return (
@@ -93,11 +101,13 @@ export default function ConfirmedList(props: { users: Record<string, User>; alar
             <div className="flex w-full flex-col gap-5">
                 <strong>Insatsstyrka</strong>
 
-                {groups.map(({ title, icon: Icon, color, data }) => (
+                {groups.map(({title, data}) => (
                     data.length > 0 && (
                         <div key={title}>
                             <div className="text-lg font-bold mt-4 flex items-center justify-center gap-2">
-                                <Icon className={`w-6 h-6 ${color}`} />{data.length}&nbsp;{title}
+                                <span className="font-light font-stretch-expanded">{title}</span>
+                                <span
+                                    className="ring-1 ring-gray-300/10 ring-inset rounded px-2">{data.length}</span>
                             </div>
                             <ul role="list" className="divide-y divide-gray-100">
                                 {data.map((confirm, i) => {
@@ -105,11 +115,18 @@ export default function ConfirmedList(props: { users: Record<string, User>; alar
                                     return (
                                         <li key={i} className="flex items-center justify-between py-3 gap-3 xl:py-5">
                                             <div className="flex flex-row gap-1 items-center">
-                                                <span className="truncate max-w-full max-w-1/3">{user?.first_name} {user?.last_name}</span>
-                                                <span>{user?.truck ? <Truck className="w-5 h-5 opacity-80" color="#4783B5" strokeWidth={1} /> : ""}</span>
-                                                <span>{user?.smoke ? <Flame className="w-5 h-5 opacity-50" fill="#FF4D00" strokeWidth={0} /> : ""}</span>
+
+                                                <span>{user?.truck ?
+                                                    <Truck className="w-5 h-5 opacity-80" color="#4783B5"
+                                                           strokeWidth={1}/> : <span className='w-5 block'/>}</span>
+                                                <span>{user?.smoke ?
+                                                    <Flame className="w-5 h-5 opacity-50" fill="#FF4D00"
+                                                           strokeWidth={0}/> : <span className='w-5 block'/>}</span>
+                                                <span
+                                                    className="truncate max-w-full max-w-1/3">{user?.first_name} {user?.last_name}</span>
+
                                             </div>
-                                            <span style={{ fontFamily: 'Courier New, monospace' }}>
+                                            <span style={{fontFamily: 'Courier New, monospace'}}>
                                                 {confirm.timeLeft > 0 ? formatTime(confirm.timeLeft) : ""}
                                             </span>
                                         </li>
