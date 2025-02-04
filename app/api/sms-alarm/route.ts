4c53b1af36785deaf320cba37c7e81f20fc6a053
Tommy <tommy.barden@lagtinget.ax>
2025-02-04 12:49:06 +0200

    Changed to named groups

--- app/api/sms-alarm/route.ts
+++ app/api/sms-alarm/route.ts
@@ -5,34 +5,39 @@ export async function POST(request: Request) {
     const apiKey = request.headers.get("x-api-key");
 
     if (!apiKey || process.env.API_KEY !== apiKey) {
-        return NextResponse.json({error: "Unauthorized", apiKey}, {status: 403});
+        return NextResponse.json({error: "Unauthorized"}, {status: 403});
     }
 
     const {sms, unit} = await request.json()
 
-    await writeToLog('SMS recieved from ' + unit, sms)
-
-    const regex = /^(N\s\d{2}[?,#]\d{1,3}\.\d{1,4}\sE\s\d{2}[?,#]\d{1,3}\.\d{1,3})?;?(.+)[;,\/]\s*(Klass:[^;]+);\s+([A-Z]{1,3}[0-9]{2,3}.*)$/;
-    const matches = sms.match(regex);
+    const regex = /^(?<geo>N\s\d{2}[?,#]\d{1,3}\.\d{1,4}\sE\s\d{2}[?,#]\d{1,3}\.\d{1,3})?;?(?<location>.+)[;,\/]\s*(?<description>Klass:[^;]+);\s+(?<units>[A-Z]{1,3}[0-9]{2,3}.*)$/;
+    const matches = sms ? sms.match(regex) : null;
     let alarm: { [key: string]: string } = {
         geo: "",
         location: "",
-        description: "",
-        units: "",
+        description: sms ?? "",
+        units: "J11, J12, J14, J15, J17, J18",
     };
 
-    if (matches) {
+    if (matches && matches?.groups) {
         alarm = {
-            geo: matches[1] || "",
-            location: matches[2] || "Okänd adress",
-            description: matches[3] || "Beskrivning saknas",
-            units: matches[4] || "",
+            geo: matches.groups.geo || "",
+            location: matches.groups.location || "Okänd adress",
+            description: matches.groups.description || "Beskrivning saknas",
+            units: matches.groups.units || "",
         }
+    } else {
+        await writeToLog('Could not parse SMS', sms ?? '')
+        // TODO: Skapa ett larm med "sms" som beskrivning ifall det går snett
+        return NextResponse.json({error: "Could not parse SMS", sms}, {status: 424});
     }
 
     alarm = Object.fromEntries(
         Object.entries(alarm).map(([key, value]: [string, string]) => [key, value.trim()])
     );
 
-    return NextResponse.json({message: "OK", alarm, unit, json: JSON.stringify(alarm)}, {status: 200});
+    // TODO: Kolla i databasen om det finns ett larm aktivt. Ifall aktivt larm finnes, uppdatera. Om ej, skapa ett
+    await writeToLog('SMS recieved from ' + unit, JSON.stringify(alarm))
+
+    return NextResponse.json({message: "OK", alarm, unit}, {status: 200});
 }
