9681d0db5e0cee9da50c9cf61a57a8b453fd44bd
tommybarden <tommy.barden@gmail.com>
2025-02-22 16:31:19 +0200

    Linted...

--- components/default/all-users.tsx
+++ components/default/all-users.tsx
@@ -7,10 +7,10 @@ export default function UsersList(props: { users: Record<string, User>; classNam
     const sortedUsers = Object.values(users).sort((a, b) => (a.last_name + a.first_name).localeCompare(b.last_name + b.first_name, 'sv'))
 
     return (
-        <div>
+        <div className={className}>
             <ul role="list" className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
                 {sortedUsers.map((person) => (
-                    <li className="flex flex-col items-center justify-center p-4 bg-gray-800 text-white shadow-md border-l-4 border-gray-200 rounded-r-xl space-y-2">
+                    <li key={person.id} className="flex flex-col items-center justify-center p-4 bg-gray-800 text-white shadow-md border-l-4 border-gray-200 rounded-r-xl space-y-2">
 
                         <div className="flex flex-row gap-2 items-center">
                             <Contact color="#ff0000" strokeWidth={1} className="w-6 h-6" />
