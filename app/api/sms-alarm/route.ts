e36e628492a6351cb8325c47b39ecc1005e7b043
tommybarden <tommy.barden@gmail.com>
2025-02-25 10:45:03 +0200

    Updated regex to match "förstärkning"

--- app/api/sms-alarm/route.ts
+++ app/api/sms-alarm/route.ts
@@ -1,23 +1,24 @@
-import {NextResponse} from "next/server";
-import {writeToLog} from "@/services/server/log";
-import {upsertAlarm} from "@/services/server/alarms";
+import { NextResponse } from "next/server";
+import { writeToLog } from "@/services/server/log";
+import { upsertAlarm } from "@/services/server/alarms";
 
 export async function POST(request: Request) {
     const apiKey = request.headers.get("x-api-key");
 
     if (!apiKey || process.env.API_KEY !== apiKey) {
-        return NextResponse.json({error: "Unauthorized"}, {status: 403});
+        return NextResponse.json({ error: "Unauthorized" }, { status: 403 });
     }
 
     try {
-        const {sms, unit} = await request.json()
+        const { sms, unit } = await request.json()
 
         if (!sms) {
-            return NextResponse.json({error: "Unauthorized"}, {status: 400});
+            return NextResponse.json({ error: "Unauthorized" }, { status: 400 });
         }
 
         //const geoRegex = /^(?<geo>N\s\d{2}[?,#]\d{1,3}\.\d{1,4}\sE\s\d{2}[?,#]\d{1,3}\.\d{1,3})$/;
-        const infoRegex = /^(?<location>.+)[;,\/]\s*(?<description>Klass:[^;]+);\s+(?<units>[A-Z]{1,3}[0-9]{2,3}.*)$/;
+        //const infoRegex = /^(?<location>.+)[;,\/]\s*(?<description>Klass:[^;]+);\s+(?<units>[A-Z]{1,3}[0-9]{2,3}.*)$/;
+        const infoRegex = /^(?<location>.+)[;,\/]\s*(?<description>(Klass:|Typ av förstärkning)[^;]+);\s+(?<units>[A-Za-z]{1,3}[0-9]{2,3}.*)$/;
         const geoRegex = /^N\s(?<lat>\d{2}[#?]\d{1,2}\.\d{1,3})\sE\s(?<lon>\d{2}[#?]\d{1,2}\.\d{1,3})$/;
 
         const geo = sms.match(geoRegex);
@@ -31,7 +32,7 @@ export async function POST(request: Request) {
         };
 
         if (geo && geo?.groups) {
-            const {lat, lon} = geo?.groups;
+            const { lat, lon } = geo?.groups;
 
             alarm.geo = [lat, lon].map(str => {
                 const [degrees, minutes] = str.replace("?", "#").split("#");
@@ -49,7 +50,7 @@ export async function POST(request: Request) {
 
         if (!geo && !info) {
             await writeToLog('Could not parse SMS', sms)
-            return NextResponse.json({error: "Could not parse SMS", sms}, {status: 424});
+            return NextResponse.json({ error: "Could not parse SMS", sms }, { status: 424 });
         }
 
         alarm = Object.fromEntries(
@@ -59,10 +60,10 @@ export async function POST(request: Request) {
         const currentAlarm = await upsertAlarm(alarm)
         await writeToLog('SMS recieved from ' + unit, JSON.stringify(alarm))
 
-        return NextResponse.json({message: "OK", currentAlarm}, {status: 200});
+        return NextResponse.json({ message: "OK", currentAlarm }, { status: 200 });
 
     } catch (e) {
         console.error("Error handling event:", e);
-        return NextResponse.json({error: e}, {status: 500});
+        return NextResponse.json({ error: e }, { status: 500 });
     }
 }
