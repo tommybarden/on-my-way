932d4a6146e9f3368094a2c4105ad60d3e578aaf
Tommy <tommy.barden@lagtinget.ax>
2025-03-13 12:27:21 +0200

    Added debug to test "after" function

---
+++ app/api/debug/route.ts
@@ -0,0 +1,27 @@
+import {after, NextResponse} from 'next/server';
+import {writeToLog} from "@/services/server/log";
+
+export async function POST(request: Request) {
+    const apiKey = request.headers.get("x-api-key");
+
+    if (!apiKey || process.env.API_KEY !== apiKey) {
+        return NextResponse.json({error: "Unauthorized"}, {status: 403});
+    }
+
+    try {
+        await writeToLog('Debug', 'Start ' + Date.now())
+
+        after(async () => {
+            const postpone = 500;
+            await new Promise(resolve => setTimeout(resolve, postpone));
+
+            await writeToLog('Debug', 'End ' + Date.now())
+        });
+
+        return NextResponse.json({message: "OK"}, {status: 200});
+
+    } catch (e) {
+        console.error("Error handling event:", e);
+        return NextResponse.json({error: e}, {status: 500});
+    }
+}
