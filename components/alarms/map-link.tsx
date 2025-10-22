806a1832b560cc0a3e484332598c0d0b32d41a5f
tommybarden <tommy.barden@gmail.com>
2025-10-22 22:35:32 +0300

    Cleanups

--- components/alarms/map-link.tsx
+++ components/alarms/map-link.tsx
@@ -6,15 +6,11 @@ import { MapPin } from "lucide-react";
 export default function MapLink(props: { geo?: string; className?: string }) {
     const { geo, className } = props
 
-    // Avbryt om geo saknas eller är tom/noll
     if (!geo || geo.trim() === '' || geo.trim() === '0') {
         return null
     }
 
-    // Konvertera "60.105 19.796583333333334" → "60.105,19.796583333333334"
     const formattedGeo = geo.trim().replace(/\s+/, ',')
-
-    // Skapa Google Maps-länk (visar platsen)
     const mapUrl = `https://www.google.com/maps?q=${formattedGeo}`
 
     const openMap = () => {
@@ -22,7 +18,7 @@ export default function MapLink(props: { geo?: string; className?: string }) {
     }
 
     return (
-        <div className={props.className + ' p-4'}>
+        <div className={className + ' p-4'}>
             <div className="flex w-full flex-col gap-5">
                 <Button onClick={openMap} type="button" variant={"outline"} size={"lg"} className="flex items-center gap-2">
                     <MapPin className="w-5 h-5" />
